2fc3ce7097d9352d1f00edb58a754e8f07e24782
AzBing <812687197@qq.com>
2024-02-09 20:48:34 +0800

    Add files via upload

---
+++ Task_1_19/Task_1_19/test.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+
+//int main()
+//{
+//	int m = 0;
+//	int i = 0;
+//	scanf("%d", &m);
+//	int max = m * (m - 1) + 1;
+//	printf("%d", max);
+//	for (i = 1; i < m; i++)
+//	{
+//		printf("+%d", max + i * 2);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	int a1 = 2;
+//	while (scanf("%d", &n) == 1)
+//	{
+//		int an = a1 + (n - 1) * 3;
+//		printf("%d\n", n * (a1 + an) / 2);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) == 1)
+//	{
+//		int count1 = 0;
+//		int count2 = 0;
+//		int tmp = 0;
+//		float sum = 0;
+//		for(int i =0;i<n;i++)
+//		{
+//			scanf("%d", &tmp);
+//			if (tmp < 0)
+//			{
+//				count1++;
+//			}
+//			else if (tmp > 0)
+//			{
+//				sum += tmp;
+//				count2++;
+//			}
+//		}
+//		printf("%d %.1lf\n", count1, sum / count2);
+//
+//	}
+//
+//	return 0;
+//}
+
+//struct s1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//
+//struct s2
+//{
+//	char c1;
+//	char c2;
+//	int i;
+//};
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(struct s1));
+//	printf("%d\n", sizeof(struct s2));
+//}
+
+
