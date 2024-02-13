f6121263b1757d7604daf8cdcec1af3e6aae6c78
AzBing <812687197@qq.com>
2024-02-13 22:40:04 +0800

    Add files via upload

---
+++ code_exercise_1_29/code_exercise_1_29/main.c
@@ -0,0 +1,108 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//int main() 
+//{
+//    double price = 0;
+//    scanf("%lf", &price);
+//    if (price >= 100 && 500 > price)
+//    {
+//        price *= 0.9;
+//    }
+//    if (price >= 500 && 2000 > price)
+//    {
+//        price *= 0.8;
+//    }
+//    if (price >= 2000 && 5000 > price)
+//    {
+//        price *= 0.7;
+//    }
+//    if (price >= 5000)
+//    {
+//        price *= 0.6;
+//    }
+//    printf("%.1lf", price);
+//    return 0;
+//}
+
+
+#include <math.h>
+//int main() 
+//{
+//    int x, y;
+//    int a, b;
+//    scanf("%d %d\n%d %d", &x, &y, &a, &b);
+//    if ((abs(a - x) + abs(b - y)) == 1)
+//    {
+//        if ((a - x) == -1)
+//        {
+//            printf("l\n");
+//        }
+//        else if ((a - x) == 1)
+//        {
+//            printf("r\n");
+//        }
+//        else if ((b - y) == 1)
+//        {
+//            printf("u\n");
+//        }
+//        else if ((b - y) == -1)
+//        {
+//            printf("d\n");
+//        }
+//    }
+//
+//    return 0;
+//}
+
+//int main() {
+//    int input = 0;
+//    scanf("%d", &input);
+//    switch (input)
+//    {
+//    case 1:
+//        printf("Monday\n");
+//        break;
+//    case 2:
+//        printf("Tuesday\n");
+//        break;
+//    case 3:
+//        printf("Wednesday\n");
+//        break;
+//    case 4:
+//        printf("Thursday\n");
+//        break;
+//    case 5:
+//        printf("Friday\n");
+//        break;
+//    case 6:
+//        printf("Saturday\n");
+//        break;
+//    case 7:
+//        printf("Sunday\n");
+//        break;
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
