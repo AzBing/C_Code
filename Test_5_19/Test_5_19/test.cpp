d2fe80f6d5e7aa210e07c693858a3f8e73e096dc
青旋 <812687197@qq.com>
2024-05-18 18:07:31 +0800

    Add files via upload

---
+++ Test_5_19/Test_5_19/test.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "BinarySearchTree.h"
+
+int main()
+{
+
+	int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+	BSTree<int> bs;
+	for (auto e : a)
+	{
+		bs.Insert(e);
+	}
+	bs.InOrder();
+
+	bs.Eraer(14);
+	bs.InOrder();
+
+	bs.Eraer(7);
+	bs.InOrder();
+
+	bs.Eraer(8);
+	bs.InOrder();
+
+	return 0;
+}
