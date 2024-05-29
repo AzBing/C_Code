8cc4545e922263eaa35afb08ce26c31d147aa45d
青旋 <812687197@qq.com>
2024-05-30 07:25:27 +0800

    Add files via upload

---
+++ Test_5_29/Test_5_29/test.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+
+#include"AVLTree.h"
+
+int main()
+{
+	const int N = 20;
+	vector<int> v;
+	v.reserve(N);
+	//srand(time(0));
+
+	for (size_t i = 0; i < N; i++)
+	{
+		v.push_back(rand());
+		cout << v.back() << endl;
+	}
+
+	AVLTree<int, int> t;
+	for (auto e : v)
+	{
+		if (e == 14604)
+		{
+			int x = 0;
+		}
+
+		t.Insert(make_pair(e, e));
+		cout << "Insert:" << e << "->" << t.IsBalance() << endl;
+	}
+
+	cout << t.IsBalance() << endl;
+
+	return 0;
+}
