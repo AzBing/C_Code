05f449e3e04f62f2097fa2d48b4681d6bc0891f4
青旋 <812687197@qq.com>
2024-06-04 13:06:42 +0800

    Add files via upload

---
+++ Test_6_3/Test_6_3/test.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#include"RBTree.h"
+
+//int main()
+//{
+//	//int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+//	int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+//	RBTree<int, int> t;
+//	for (auto e : a)
+//	{
+//		t.Insert(make_pair(e, e));
+//	}
+//	t.InOrder();
+//
+//	cout << t.IsBalance() << endl;
+//
+//	return 0;
+//}
+
+int main()
+{
+	const int N = 10;
+	vector<int> v;
+	v.reserve(N);
+	srand(time(0));
+
+	for (size_t i = 0; i < N; i++)
+	{
+		v.push_back(rand() + i);
+		//cout << v.back() << endl;
+	}
+
+	size_t begin2 = clock();
+	RBTree<int, int> t;
+	for (auto e : v)
+	{
+
+		t.Insert(make_pair(e, e));
+		cout << "Insert:" << e << "->" << t.IsBalance() << endl;
+	}
+	size_t end2 = clock();
+
+	cout << "Insert:" << end2 - begin2 << endl;
+
+	cout << t.IsBalance() << endl;
+
+	return 0;
+}
