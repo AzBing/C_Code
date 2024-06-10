90905916f1089e1b29bc6fab1c289bfddebea0c2
青旋 <812687197@qq.com>
2024-06-10 11:43:50 +0800

    Add files via upload

---
+++ Test_6_8/Test_6_8/HashTable.h
@@ -0,0 +1,68 @@
+#pragma once
+#include<vector>
+
+namespace bit
+{
+	enum Status
+	{
+		EMPTY,
+		EXIST,
+		DELETE
+	};
+
+	template<class K, class V>
+	struct HashData
+	{
+		pair<K, V> _kv;
+		Status _s;          //状态
+	};
+
+	template<class K, class V>
+	class HashTable
+	{
+	public:
+		HashTable()
+		{
+			_tables.resize(10);
+		}
+
+		bool Insert(const pair<K, V>& kv)
+		{
+			// 负载因子0.7就扩容
+			if (_n * 10 / _tables.size() == 7)
+			{
+				size_t newSize = _tables.size() * 2;
+				HashTable<K, V> newHT;
+				newHT._tables.resize(newSize);
+				// 遍历旧表
+				for (size_t i = 0; i < _table.size(); i++)
+				{
+					if (_tables[i]._s == EXIST)
+					{
+						newHT.Insert(_tables[i]._kv);
+					}
+				}
+
+				_tables.swap(newHT._tables);
+			}
+
+			size_t hashi = kv.first % _tables.size();
+			while (_tables[hashi]._s == EXIST)
+			{
+				hashi++;
+
+				hashi %= _tables.size();
+			}
+
+			_tables[hashi]._kv = kv;
+			_tables[hashi]._s = EXIST;
+			++_n;
+
+			return true;
+		}
+
+	private:
+		vector<HashData> _tables;
+		size_t _n = 0; // 存储的关键字的个数
+	};
+}
---
+++ Test_6_8/Test_6_8/test.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+
+using namespace std;
+
+
+int main()
+{
+
+
+	return 0;
+}
