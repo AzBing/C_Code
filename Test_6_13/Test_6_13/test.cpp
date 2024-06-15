61f49409b43f35b00eb43acc74d3a109cd397c1c
青旋 <812687197@qq.com>
2024-06-15 10:03:02 +0800

    Add files via upload

---
+++ Test_6_13/Test_6_13/test.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<string>
+#include<unordered_set>
+#include<unordered_map>
+#include<map>
+#include<set>
+using namespace std;
+
+#include"HashTable.h"
+
+
+
+#include"MyUnorderedMap.h"
+#include"MyUnorderedSet.h"
+
+int main()
+{
+	//hash_bucket::TestHT1();
+	//hash_bucket::TestHT3();
+
+	bit::test_map();
+	bit::test_set();
+
+
+	return 0;
+}
