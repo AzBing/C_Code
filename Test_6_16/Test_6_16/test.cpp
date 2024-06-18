d22dbae4bbe3d51019bee429296f94915d4f0a30
青旋 <812687197@qq.com>
2024-06-18 10:18:38 +0800

    Add files via upload

---
+++ Test_6_16/Test_6_16/test.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+#include<unordered_set>
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<bitset>
+using namespace std;
+#include"BloomFilter.h"
+
+int main()
+{
+	TestBF2();
+
+	return 0;
+}
+
+
+
