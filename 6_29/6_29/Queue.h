36ce7a570ed26235d3ee9e4b1d3433e00fdfe7d7
青旋 <812687197@qq.com>
2024-06-30 19:06:14 +0800

    Add files via upload

---
+++ 6_29/6_29/Queue.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace bit
+{
+	void QueueInit()
+	{
+		cout << "void QueueInit()" << endl;
+	}
+
+	namespace bit1
+	{
+		int rand = 1;
+	}
+}
---
+++ 6_29/6_29/Stack.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<stdlib.h>
+
+namespace bit
+{
+	/*void StackInit()
+	{
+		cout << "void StackInit()" << endl;
+	}
+
+	namespace bit2
+	{
+		int rand = 0;
+	}*/
+
+	typedef struct Stack
+	{
+		int* a;
+		int top;
+		int capacity;
+	}ST;
+
+	// 不允许声明和定义同时给缺省参数
+	// 声明给，定义不给
+	void StackInit(ST* ps, int N = 4);
+	void StackPush(ST* ps, int x);
+}
