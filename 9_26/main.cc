61291cb421f62c0b9e2b712660c2d50e6f5649dc
青旋 <812687197@qq.com>
2024-09-28 23:26:20 +0800

    Add files via upload

---
+++ 9_26/main.cc
@@ -0,0 +1,18 @@
+#include "Udpserver.hpp"
+#include <memory>
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2){
+		std::cerr << "Usage: " << argv[0] << " port" << std::endl;
+		return 1;
+	}
+	std::string ip = "127.0.0.1"; //本地环回
+	int port = atoi(argv[1]);
+	UdpServer* svr = new UdpServer(ip, port);
+	svr->InitServer();
+	svr->Start();
+	return 0;
+}
+
+
