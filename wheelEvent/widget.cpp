c1f59cfe53ec00d8f3cddf229dca9d483aafa34e
青旋 <812687197@qq.com>
2025-01-10 07:07:37 +0800

    Add files via upload

---
+++ wheelEvent/widget.cpp
@@ -0,0 +1,23 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QtDebug>
+#include <QWheelEvent>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    total = 0;
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::wheelEvent(QWheelEvent *event)
+{
+    total += event->delta();
+    qDebug() << total;
+}
