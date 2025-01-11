b84b8fee85dc479def6394e072e868f21851bf4a
青旋 <812687197@qq.com>
2025-01-11 14:59:14 +0800

    Add files via upload

---
+++ keyPressEvent/widget.cpp
@@ -0,0 +1,22 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QKeyEvent>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::keyPressEvent(QKeyEvent *event)
+{
+    qDebug() << event->key();
+}
+
