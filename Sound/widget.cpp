e4da290475857eed5d6aa88d55ab3955b7c2de80
青旋 <812687197@qq.com>
2025-01-21 10:58:54 +0800

    Add files via upload

---
+++ Sound/widget.cpp
@@ -0,0 +1,23 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    sound = new QSound(":/di.wav", this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    // 处理音频播放操作
+    sound->play();
+}
